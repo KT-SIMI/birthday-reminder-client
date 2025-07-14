3d11aa7bad1e6640b85ac14f2aa3727899d120fd
Ikeoluwakiitan Oyewole <uthmanoyewole95@gmail.com>
2025-07-14 22:47:42 +0100

    ...

--- src/components/AuthLink.jsx
+++ src/components/AuthLink.jsx
@@ -2,13 +2,12 @@ export default function AuthLink({ text, linkText, onClick }) {
   return (
     <div className="text-center mt-6 text-[#718096] text-sm">
       {text}{" "}
-      <a
-        href={""}
+      <button
         onClick={onClick}
         className="text-[#667eea] no-underline font-semibold transition-all duration-300 ease-[ease] hover:text-[#764ba2]"
       >
         {linkText}
-      </a>
+      </button>
     </div>
   );
 }
