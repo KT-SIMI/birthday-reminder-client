abff98b9d122d3f4962e7bd8739d106a0c32aef8
Ikeoluwakiitan Oyewole <uthmanoyewole95@gmail.com>
2025-07-14 02:53:04 +0100

    ...

--- src/components/birthdayMessageSlides/Greeting.jsx
+++ src/components/birthdayMessageSlides/Greeting.jsx
@@ -1,10 +1,10 @@
 export default function Greeting({ friend }) {
    return (
-    <div className="bg-gradient-to-br from-[#d946ef] to-[#8b5cf6] slide">
+    <div className="bg-gradient-to-br from-[#2dd4bf] to-[#06b6d4] slide">
         <div className="text-[4rem] font-bold mb-8 animate-[bounceIn_1s_ease-out_0.5s_both]">
             Hey
         </div>
-        <div className="text-[5rem] font-black bg-gradient-to-r from-[#fde047] via-[#fdba74] to-[#fca5a5] bg-clip-text text-transparent animate-[glow_2s_ease-in-out_infinite_alternate] [text-shadow:_0_0_30px_rgba(255,215,0,0.5)]">
+        <div className="text-[5rem] font-black text-[#64748b]  animate-[glow_2s_ease-in-out_infinite_alternate]">
             {friend.name}
         </div>
     </div>
