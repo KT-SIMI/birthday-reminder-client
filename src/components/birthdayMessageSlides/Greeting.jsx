624c9048032f1e296ac599a5baf5c1bdb03164e6
Ikeoluwakiitan Oyewole <uthmanoyewole95@gmail.com>
2025-07-18 00:35:56 +0100

    ..

--- src/components/birthdayMessageSlides/Greeting.jsx
+++ src/components/birthdayMessageSlides/Greeting.jsx
@@ -2,7 +2,7 @@ export default function Greeting({ friend }) {
    return (
     <div className="bg-gradient-to-br from-[#2dd4bf] to-[#06b6d4] slide">
         <div className="text-[4rem] font-bold mb-8 animate-[bounceIn_1s_ease-out_0.5s_both]">
-            Hey
+            Hi
         </div>
         <div className="text-[5rem] font-black text-[#475569]  animate-[heartbeat_0.6s_ease-in-out_indefinite">
             {friend.name}
